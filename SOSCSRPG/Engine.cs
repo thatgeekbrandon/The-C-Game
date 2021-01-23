38d468a75f8525e653c8797d9c4af03bda67ffc7
thatgeekbrandon <68824480+thatgeekbrandon@users.noreply.github.com>
2021-01-22 19:31:21 -0700

    Initial commit
    XAML Grid set up

---
+++ SOSCSRPG/Engine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Engine
+{
+	public Engine()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
